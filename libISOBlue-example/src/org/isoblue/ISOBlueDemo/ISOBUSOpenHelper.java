3d3b6b2b7f0c24980496d0d534b3be60a4a00953
Alex Layton <awlayton@purdue.edu>
2013-09-30 19:22:24 -0400

    Moved SQLite database for demo app to SD Card.

--- libISOBlue-example/src/org/isoblue/ISOBlueDemo/ISOBUSOpenHelper.java
+++ libISOBlue-example/src/org/isoblue/ISOBlueDemo/ISOBUSOpenHelper.java
@@ -1,40 +1,45 @@
 package org.isoblue.ISOBlueDemo;
 
+import java.io.File;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
 
 public class ISOBUSOpenHelper extends SQLiteOpenHelper {
 
 	public static final String DATABASE_NAME = "isobus_messages.db";
-    public static final String TABLE_MESSAGES = "isobus_messages";
-    public static final String COLUMN_ID = "id";
-    public static final String COLUMN_PGN = "pgn";
-    public static final String COLUMN_DATA = "data";
-    public static final String COLUMN_SRC = "src";
-    public static final String COLUMN_DEST = "dest";
-    public static final String COLUMN_BUS = "bus";
-    public static final String COLUMN_TIME = "time";
+	public static final String TABLE_MESSAGES = "isobus_messages";
+	public static final String COLUMN_ID = "id";
+	public static final String COLUMN_PGN = "pgn";
+	public static final String COLUMN_DATA = "data";
+	public static final String COLUMN_SRC = "src";
+	public static final String COLUMN_DEST = "dest";
+	public static final String COLUMN_BUS = "bus";
+	public static final String COLUMN_TIME = "time";
 
-    private static final int DATABASE_VERSION = 1;
-    private static final String MESSAGE_TABLE_CREATE =
-                "CREATE TABLE " + TABLE_MESSAGES + " (" +
-                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                COLUMN_PGN + " INTEGER, " +
-                COLUMN_DATA + " BLOB, " +
-                COLUMN_SRC + " INTEGER, " +
-                COLUMN_DEST + " INTEGER, " +
-                COLUMN_BUS + " TEXT, " +
-                COLUMN_TIME + " INTEGER);";
+	private static final int DATABASE_VERSION = 1;
+	private static final String MESSAGE_TABLE_CREATE = "CREATE TABLE "
+			+ TABLE_MESSAGES + " (" + COLUMN_ID
+			+ " INTEGER PRIMARY KEY AUTOINCREMENT, " + COLUMN_PGN
+			+ " INTEGER, " + COLUMN_DATA + " BLOB, " + COLUMN_SRC
+			+ " INTEGER, " + COLUMN_DEST + " INTEGER, " + COLUMN_BUS
+			+ " TEXT, " + COLUMN_TIME + " INTEGER);";
 
-    ISOBUSOpenHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+	ISOBUSOpenHelper(Context context) {
+		super(context, Environment.getExternalStorageDirectory()
+				+ File.separator + DATABASE_NAME, null, DATABASE_VERSION);
+		MediaScannerConnection.scanFile(context,
+				new String[] { Environment.getExternalStorageDirectory()
+						+ File.separator + DATABASE_NAME }, null, null);
+	}
 
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(MESSAGE_TABLE_CREATE);
-    }
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(MESSAGE_TABLE_CREATE);
+	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
