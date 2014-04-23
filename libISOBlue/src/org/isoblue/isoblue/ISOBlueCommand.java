91b9cfd61dea9d66cc6acc8c637db680c86fb7f7
Alex Layton <alex@layton.in>
2014-04-22 21:59:44 -0400

    Fix "fix" from 46bb388

--- libISOBlue/src/org/isoblue/isoblue/ISOBlueCommand.java
+++ libISOBlue/src/org/isoblue/isoblue/ISOBlueCommand.java
@@ -89,10 +89,10 @@ public final class ISOBlueCommand {
     public ISOBlueCommand(OpCode opCode, byte bus, byte sock, byte data[]) {
         mOpCode = opCode;
         // Check bus fits into one nibble
-        if(bus > 127 || bus < -128) {
+        if(bus > 7 || bus < -8) {
             throw new IllegalArgumentException("bus must only be one nibble, given " + bus);
         }
-        mBus = bus;
+        mBus = (byte) (bus & 0x0F);
         mSock = sock;
 
         mData = new byte[data.length];
@@ -129,7 +129,7 @@ public final class ISOBlueCommand {
         s = new StringBuilder();
 
         s.append(this.mOpCode.val);
-        s.append(String.format("%1x", this.mBus).substring(0, 1));
+        s.append(String.format("%1x", this.mBus));
         s.append(new String(this.mData));
 
         return s.toString();
